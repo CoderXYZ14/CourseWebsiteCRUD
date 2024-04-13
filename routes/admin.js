06496d29725a78a8a15930f4e8cb8ecffe90a406
CoderXYZ14 <s1404islam@gmail.com>
2024-04-13 21:48:10 +0530

    Completed

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,50 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  await Admin.create({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "Admoini created successfully",
+  });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
-});
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageLink = req.body.imageLink;
+  const price = req.body.price;
+  const newCourse = await Course.create({
+    title,
+    description,
+    imageLink,
+    price,
+  });
+  //we want to send the course id as well
+  //the unique id generated from mongo db compass
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+  res.json({
+    message: "Cpurse created successfully",
+    courseId: newCourse._id,
+  });
+});
+//all authentication logic taken care by admin MiddleWare
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const response = await Course.find({}); //send all courses
+  res.json({
+    courses: response,
+  });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,64 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  User.create({
+    username,
+    password,
+  });
+  res.json({ message: "User created successfully" });
+});
+//no user middleware as it is open to the word
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const response = await Course.find({}); //send all courses
+  res.json({
+    courses: response,
+  });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+//cant be send directly as we have desided to send reference of purchased course
+//else would be direct Purchase.find()
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const courseId = req.params.courseId;
+  const username = req.headers.username;
+  await User.updateOne(
+    {
+      username: username,
+    },
+    {
+      $push: {
+        purchasedCourses: courseId,
+      },
+    }
+  );
+  res.json({
+    msg: "purchase complete",
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.headers.username,
+  });
+
+  //console.log(user.purchasedCourses); //logs an array of purchased courses
+  const courses = await Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+  res.json({
+    courses: courses,
+  });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
-});
-
-module.exports = router
+module.exports = router;
